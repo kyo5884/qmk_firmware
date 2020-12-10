a9ba69ead82fc5f96c00a40bb20514cf81962cca
kyo5884 <kyo5884.public@gmail.com>
2020-12-10 18:49:11 +0900

    fix column pin order

--- keyboards/kyo5884/sidebar0210/config.h
+++ keyboards/kyo5884/sidebar0210/config.h
@@ -12,6 +12,6 @@
 #define MATRIX_COLS 2
 
 #define MATRIX_ROW_PINS { F6, F7, B1, B3, B2, B6 }
-#define MATRIX_COL_PINS { B4, B5 }
+#define MATRIX_COL_PINS { B5, B4 }
 #define UNUSED_PINS { D4, C6, D7, F4, F5 }
 #define DIODE_DIRECTION COL2ROW
