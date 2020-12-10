315d934d3361f2c1c345104f93f0f51e0d996396
kyo5884 <kyo5884.public@gmail.com>
2020-12-10 12:53:37 +0900

    fix row count

--- keyboards/kyo5884/sidebar0210/config.h
+++ keyboards/kyo5884/sidebar0210/config.h
@@ -8,10 +8,10 @@
 #define MANUFACTURER kyo5884
 #define PRODUCT sidebar0210
 
-#define MATRIX_ROWS 7
+#define MATRIX_ROWS 6
 #define MATRIX_COLS 2
 
-#define MATRIX_ROW_PINS { F5, F6, F7, B1, B3, B2, B6 }
+#define MATRIX_ROW_PINS { F6, F7, B1, B3, B2, B6 }
 #define MATRIX_COL_PINS { B4, B5 }
-#define UNUSED_PINS { D4, C6, D7, F4 }
+#define UNUSED_PINS { D4, C6, D7, F4, F5 }
 #define DIODE_DIRECTION COL2ROW
