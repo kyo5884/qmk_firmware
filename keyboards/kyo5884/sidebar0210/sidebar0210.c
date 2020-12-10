be55fabc6f32e934e962f87f44d4c82f14c50874
kyo5884 <kyo5884.public@gmail.com>
2020-12-10 12:55:04 +0900

    add matrix scan support

--- keyboards/kyo5884/sidebar0210/sidebar0210.c
+++ keyboards/kyo5884/sidebar0210/sidebar0210.c
@@ -1,5 +1,13 @@
 #include "sidebar0210.h"
 
+void matrix_init_kb(void) {
+	matrix_init_user();
+}
+
+void matrix_scan_kb(void) {
+	matrix_scan_user();
+}
+
 bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
 	return process_record_gfx(keycode, record) && process_record_user(keycode, record);
 }
