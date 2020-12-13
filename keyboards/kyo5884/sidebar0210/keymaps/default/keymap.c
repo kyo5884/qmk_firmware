afbaded1aa80be713fdae4dafc3e50b7716ceeac
kyo5884 <kyo5884.public@gmail.com>
2020-12-13 21:41:21 +0900

    show DRAG_DIRECTION when push and turn encoders

--- keyboards/kyo5884/sidebar0210/keymaps/default/keymap.c
+++ keyboards/kyo5884/sidebar0210/keymaps/default/keymap.c
@@ -11,13 +11,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   )
 };
 
+char DRAG_DIRECTION = 0x00;
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  if (keycode == KC_BTN1) {
+    if (record->event.pressed) {
+      // Do something when pressed
+    } else {
+      // Do something else when release
+      DRAG_DIRECTION = 0x00;
+    }
+  }
+
   return true;
 }
 
 #ifdef OLED_DRIVER_ENABLE
 void oled_task_user(void) {
-    oled_write_ln_P(PSTR("Hello, World! \\(^o^)/"), false);
+  oled_write_char(DRAG_DIRECTION, false);
 }
 #endif
 
@@ -26,11 +37,23 @@ void matrix_init_user(void) {}
 void matrix_scan_user(void) {}
 
 void encoder_update_user(uint8_t index, bool clockwise) {
-  if (index == 0) { /* Left encoder */
-    if (clockwise) tap_code(KC_MS_UP);
-    else tap_code(KC_MS_DOWN);
-  } else if (index == 1) { /* Right encoder */
-    if (clockwise) tap_code(KC_MS_RIGHT);
-    else tap_code(KC_MS_LEFT);
+  if (index == 0) {
+    /* Left encoder */
+    if (clockwise) {
+      tap_code(KC_MS_UP);
+      DRAG_DIRECTION = 0x18;
+    } else {
+      tap_code(KC_MS_DOWN);
+      DRAG_DIRECTION = 0x19;
+    }
+  } else if (index == 1) {
+    /* Right encoder */
+    if (clockwise) {
+      tap_code(KC_MS_RIGHT);
+      DRAG_DIRECTION = 0x1A;
+    } else {
+      tap_code(KC_MS_LEFT);
+      DRAG_DIRECTION = 0x1B;
+    }
   }
 }
